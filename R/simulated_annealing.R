f8e77329a2f780fd6d3ed7cc084bbd02a5b6d387
Anthony Raborn <anthony.w.raborn@gmail.com>
2018-05-03 00:03:25 -0400

    Initial SA algorithm.

---
+++ R/simulated_annealing.R
@@ -0,0 +1,86 @@
+##### Simulated Annealing Outline ####
+
+# initialModel - the initial, full form
+# currentModel - the model of the current step
+# maxSteps - the number of steps (iterations)
+# currentStep - the current step
+# currentTemp - the current temperature. A function of the number of steps (such that temp = 0 at maxSteps), and values that control the shape of the overall temperature. Should be modifiable. At the least, needs a maxTemp value and values that control the shape (up to a cubic value, maybe?)
+# randomNeighbor - a function that determines how the form is changed at each step. Probably similar to the Tabu model. Should be able to change one or more parameters
+# goal - a function that determines the "goodness" of the currentModel. Typically in SA goodness is defined as minimization! Sometimes called an energ function
+# selectionFunction - a function that determines if a randomNeighbor change is accepted. Uses the goal function that determines the "goodness" of the currentModel and the "goodness" of the randomNeighbor, and the currentTemp to generate a probability of acceptance, then compares this probability to a U[0,1] variable to determine if accepted or not. A standard version of this is P(goal, goal', currentTemp) = [(1 if goal' better than goal), (exp(-(goal' - goal)/currentTemp) if goal' worse than goal)] (citation: Kirkpatrick et al., 1983). Allowing other functions would be ideal.
+# bestModel - the model with the best value of the goal function achieved so far
+# bestGoal - the best value of the goal function achieved so far
+# restartCriteria - if allowed, this would "restart" the SA process by changing currentModel to bestModel and continuing the process. Could be based on (1) the currentStep value, (2) the difference between goal(currentModel) and goal(bestModel), (3) randomness (i.e., could randomly restart, could randomly restart based on some values, etc), (4) other critera.
+
+simulatedAnnealing <- function(initialModel, originalData, maxSteps, fitStatistic = 'cfi', temperature = "linear", maximize = TRUE, Kirkpatrick = TRUE, randomNeighbor = TRUE, maxChanges = 5, restartCriteria = "consecutive", ...){
+  
+  currentModel = bestModel = initialModel
+  currentStep = 1
+  consecutive = 0
+  bestFit = 0
+
+  # if(...)
+  
+  if (temperature == "linear") {
+    temperatureFunction <- linearTemperature
+  } else if (temperature == "quadratic") {
+    temperatureFunction <- quadraticTemperature
+  } else if (temperature == "logistic") {
+    temperatureFunction <- logisticTemperature
+  } else if (class(temperature == "function")) {
+    temperatureFunction <- temperature
+  } else {
+    stop(
+      "You need to specify an appropriate default temperature (one of \"linear\", \"quadratic\", or \"logistic\") or include a temperature in the form of a function to continue."
+    )
+  }
+  
+  if (Kirkpatrick == TRUE) {
+    selectionFunction <- selectionFunction
+  } else if (class(Kirkpatrick == "function")) {
+    selectionFunction <- Kirkpatrick
+  } else {
+    stop(
+      "You need to specify a selection function, or set Kirkpatrick = TRUE to use the standard selection function."
+    )
+  }
+  
+  if (randomNeighbor == TRUE) {
+    randomNeighbor <- randomNeighborSelection
+  } else if (class(randomNeighbor == "function")) {
+    randomNeighbor <- randomNeighbor
+  } else{
+    stop("You need to specify a random neighbor function, or set randomNeighbor=TRUE to use the standard random neighbor function.")
+  }
+  
+  if (restartCriteria == "consecutive") {
+    restartCriteria <- consecutiveRestart
+  } else if (class(restartCriteria) == "function") {
+    restartCriteria = restartCriteria
+  } else {
+    restartCriteria = function() {}
+    warning(
+      "The restart criteria should to be either \"consecutive\" (the default) or a custom function. It has been set to NULL so the algorithm will not restart at all."
+    )
+  }
+  
+  while (currentStep < maxSteps) {
+    # how many changes to make?
+    numChanges = sample(1:maxChanges, size = 1)
+    # generate random model
+    randomNeighborModel = randomNeighbor(currentModelObject = currentModel, numChanges = numChanges, data = originalData)
+    # select between random model and current model
+    currentModel = selectionFunction(currentModel = currentModel, randomNeighborModel = randomNeighborModel, currentTemp = temperatureFunction(currentStep, maxSteps), maximize = maximize)
+    # check for current best model
+    bestModel = checkModels(currentModel, fitStatistic, maximize, bestFit)
+    bestFit = lavaan::fitmeasures(object = bestModel, fit.measures = fitStatistic)
+    # restart if the same model was chosen too many times
+    restartCriteria()
+    currentStep = currentStep + 1
+  }
+  
+  return(list(bestModel, bestFit))
+  }
+
+trial <- simulatedAnnealing(initialModel = lavaan::cfa(model = antModel, data = data),
+                            originalData = data, maxSteps = 200)
---
+++ R/simulated_annealing_internals.R
@@ -0,0 +1,151 @@
+
+
+randomNeighborSelection <- function(currentModelObject = currentModel, numChanges = numChanges, data) {
+  
+  if (class(currentModelObject) == "list") {
+    currentModelObject = currentModelObject[[1]]
+  }
+  
+  # using lavaan functions, construct a full parameter table
+  paramTable <- lavaan::parTable(currentModelObject)
+  # fullParamTable <- lavaan:::lav_partable_full(paramTable)
+  # currentModelParams <- lavaan::lav_partable_merge(paramTable, fullParamTable, remove.duplicated = TRUE, warn = FALSE)
+  
+  # select the rows that correspond to parameters related to the latent variables
+  latentVariables <- row.names(lavaan::inspect(currentModelObject, "cor.lv"))
+  currentModelParamsLV <- paramTable[paramTable$lhs %in% latentVariables,]
+  
+  # randomly select rows to make changes to
+  randomChangesRows <- sample(currentModelParamsLV$id, size = numChanges)
+  changeParamTable <- paramTable[randomChangesRows,]
+  
+  # make the changes. If currently free, fix to 0; if fixed to 0, set to free
+  paramTable$free[randomChangesRows] <- 1 - paramTable$free[randomChangesRows]
+  
+  # remove the starting value, estimates, and standard errors of the currentModel
+  paramTable$est <- NULL
+  paramTable$se <- NULL
+  paramTable$start <- NULL
+  paramTable$labels <- NULL
+  
+  # refit the model
+  prevModel <- as.list(currentModelObject@call)
+  prevModel$model <- paramTable
+  # randomNeighborModel <- try(do.call(eval(parse(text = "lavaan::lavaan")), prevModel[-1]), silent = TRUE)
+  
+  randomNeighborModel <- modelWarningCheck(
+    lavaan::lavaan(
+      model = prevModel$model,
+      data = data
+    )
+  )
+  
+  return(randomNeighborModel)
+}
+
+
+selectionFunction <- function(currentModelObject = currentModel, randomNeighborModel, currentTemp, maximize){
+  
+  # check if the randomNeighborModel is a valid model for use
+  if (length(randomNeighborModel[[2]]) > 0 | length(randomNeighborModel[[2]] > 0)) {
+    return(currentModel)
+  }
+  
+  if (class(currentModelObject) == "list") {
+    currentModelObject = currentModelObject[[1]]
+  }
+  
+  
+  # this is the Kirkpatrick et al. method of selecting between currentModel and randomNeighborModel
+  if (goal(randomNeighborModel$lavaan.output, fitStatistic, maximize) < goal(currentModelObject, fitStatistic, maximize)) {
+    
+    consecutive = consecutive + 1
+    return(randomNeighborModel)
+    
+  } else {
+    
+    probability = exp(-(goal(randomNeighborModel$lavaan.output, fitStatistic, maximize) - goal(currentModelObject, fitStatistic, maximize)) / currentTemp)
+    
+    if (probability > runif(1)) {
+      newModel = randomNeighborModel$lavaan.output
+    } else { 
+      newModel = currentModelObject
+    }
+    
+    consecutive = ifelse(identical(lavaan::parameterTable(newModel), 
+                                   lavaan::parameterTable(currentModelObject)), 
+                         consecutive + 1, 0)
+    return(newModel)
+  }
+}
+
+goal <- function(x, fitStatistic = 'cfi', maximize) {
+  
+  # if using lavaan and a singular fit statistic,
+  if (class(x) == "lavaan" & is.character(fitStatistic) & length(fitStatistic) == 1) {
+    energy <- lavaan::fitMeasures(x, fit.measures = fitStatistic)
+    
+    # if trying to maximize a value, return its negative
+    if (maximize == TRUE) {
+      return(-energy)
+    } else {
+      # if minimizing a value, return the value
+      return(energy)
+    }
+  }
+}
+
+consecutiveRestart <- function(maxConsecutiveSelection = 25){
+  if (consecutive == maxConsecutiveSelection) {
+    currentModel = bestModel
+    consecutive = 0
+  }
+}
+
+linearTemperature <- function(currentStep, maxSteps) {
+  currentTemp <- (maxSteps - currentStep)/maxSteps
+}
+
+quadraticTemperature <- function(currentStep, maxSteps) {
+  currentTemp <- ((maxSteps - currentStep)/maxSteps)^2
+}
+
+logisticTemperature <- function(currentStep, maxSteps) {
+  x = 1:maxSteps
+  x.new = scale(x, center = T, scale = maxSteps/12)
+  currentTemp <- 1/(1 + exp((x.new[currentStep])))
+}
+
+checkModels <- function(currentModel, fitStatistic, maximize = maximize, bestFit = bestFit) {
+  if (class(currentModel) == "list") {
+    currentModel <- currentModel[[1]]
+  }
+  currentFit = lavaan::fitmeasures(object = currentModel, fit.measures = fitStatistic)
+  if (maximize == TRUE) {
+    if (currentFit > bestFit) {
+      bestFit = currentFit
+      bestModel = currentModel
+    }
+  } else {
+    if (currentFit < bestFit) {
+      bestFit = currentFit
+      bestModel = currentModel
+    }
+  }
+  
+  return(bestModel)
+}
+
+modelWarningCheck <- function(expr) {
+  warn <- err <- c()
+  value <- withCallingHandlers(
+    tryCatch(expr, error=function(e) {
+      err <<- append(err, regmatches(paste(e), gregexpr("ERROR: [A-z ]{1,}", paste(e))))
+      NULL
+    }), warning=function(w) {
+      warn <<- append(warn, regmatches(paste(w), gregexpr("WARNING: [A-z ]{1,}", paste(w))))
+      invokeRestart("muffleWarning")
+    })
+  list(lavaan.output = value, warnings <- as.character(unlist(warn)), errors <- as.character(unlist(err)))
+}
+
