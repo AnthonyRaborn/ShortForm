b84fa93a2e0b92326c36195ddb55ecc70a526e67
Anthony Raborn <anthony.w.raborn@gmail.com>
2018-10-08 21:47:52 -0400

    Added S3 plot method for class "antcolony"

--- R/antcolony_plot.R
+++ R/antcolony_plot.R
@@ -1,52 +1,7 @@
-#' ACO Plot Function
-#'
-#' @param results The output from `antcolony.lavaan`.
-#' @param verbose Logical. If true, prints the warnings and errors from `ggplot2`
-#' regarding the plots being created. Defaults to `FALSE`.
-#' @param ... Other arguments to be passed to the `ggplot2` functions. Currently
-#' unimplemented.
-#'
-#' @return A list of three plots: 
-#' \describe{
-#' \item{1}{A pheromone plot, showing how the pheromone levels for each item change 
-#' for each run of the algorithm;}
-#' \item{2}{A mean gamma plot, showing a loess smoothing plot of changes in the mean
-#' gamma (regression coefficients) of the best-fit model for each run of the 
-#' algorithm;}
-#' \item{3}{A mean variance explained plot, showing a loess smoothing plot of 
-#' changes in the mean variance explained of the best-fit model for each run of the 
-#' algorithm}
-#' }
-#' 
 #' @export
 #' 
-#' @examples \dontrun{
-#' # Below is a modified version of the example in the `antcolony.lavaan` function.
-#' # The latent variables are allowed to be correlated for identification
-#' results = antcolony.lavaan(data = lavaan::HolzingerSwineford1939,
-#' ants = 5, evaporation = 0.7, 
-#' antModel = ' visual  =~ x1 + x2 + x3
-#'              textual =~ x4 + x5 + x6
-#'              speed   =~ x7 + x8 + x9 
-#'              visual ~ textual + speed
-#'              textual ~ speed', 
-#' list.items = list(c('x1', 'x2', 'x3'), 
-#'                   c('x4', 'x5', 'x6'), 
-#'                   c('x7', 'x8', 'x9')), 
-#'                   full = 9, i.per.f = c(2,2,2), 
-#'                   factors = c('visual','textual','speed'), 
-#'                   steps = 10, fit.indices = c('cfi'), 
-#'                   fit.statistics.test = "(cfi > 0.6)", 
-#'                   summaryfile = NULL, feedbackfile = NULL, max.run = 100)
-#' 
-#' # once converged, put the entire results object into the antcolony_plot function:
-#' plots <- antcolony_plot(results)
-#' 
-#' # print the plot for changes in mean gamma
-#' plots[[2]]
-#'
-#'}
-plot.antcolony <- function(results, verbose = FALSE, ...) {
+
+plot.antcolony <- function(results, verbose = FALSE, type = "all") {
   summary_results <- results[[2]]
   item_pheromone_names <-
     grep("Pheromone", names(summary_results), value = TRUE)
@@ -58,7 +13,7 @@ plot.antcolony <- function(results, verbose = FALSE, ...) {
       value = "Pheromone",
       item_pheromone_names
     )
-  
+  if (type %in% c("all", "pheromone")) {
   pheromone_plot <- ggplot2::ggplot(
     pheromone_long,
     ggplot2::aes_string(
@@ -83,7 +38,9 @@ plot.antcolony <- function(results, verbose = FALSE, ...) {
         hjust = .5
       )
     )
+  }
   
+  if (type %in% c("all", "gamma")) {
   gamma_plot <-
     ggplot2::ggplot(summary_results,
                     ggplot2::aes_string(x = "run", y = "mean.gamma")) +
@@ -103,7 +60,9 @@ plot.antcolony <- function(results, verbose = FALSE, ...) {
         hjust = .5
       )
     )
+  }
   
+  if (type %in% c("all", "variance")) {
   variance_plot <-
     ggplot2::ggplot(summary_results,
                     ggplot2::aes_string(x = "run", y = "mean.var.exp")) +
@@ -123,6 +82,7 @@ plot.antcolony <- function(results, verbose = FALSE, ...) {
         hjust = .5
       )
     )
+  }
   
   plots <- list(pheromone_plot, gamma_plot, variance_plot)
   if (verbose) {
