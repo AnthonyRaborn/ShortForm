cdf7d973eaaae923fb82017bf49254805d5973d0
Anthony Raborn <araborn@pasco.k12.fl.us>
2019-12-06 13:04:22 -0500

    New S4 classes for each algorithm

---
+++ R/SA.R
@@ -0,0 +1,113 @@
+#' An S4 class for the Simulated Annealing Algorithm
+#'
+#' @slot function_call The original function call.
+#' @slot all_fit A summary `vector` indicating the model fit results for
+#' each iteration.
+#' @slot best_fit The best model fit result using the selected `fitStatistic`. 
+#' @slot best_model A `lavaan` object of the final solution.
+#' @slot best_syntax A `character` vector of the final solution model syntax.
+#' @slot runtime A `difftime` object of the total run time of the function.
+#'
+#' @importFrom methods new
+#'
+#' @return An S4 object of class `SA`.
+#' @export
+#'
+setClass('SA',
+         slots =
+           list(
+             function_call = 'call',
+             all_fit = 'vector',
+             best_fit = 'numeric',
+             best_model = 'lavaan',
+             best_syntax = 'character',
+             runtime = 'ANY'
+           )
+)
+
+#' Print method for class `SA`
+#' @export
+setMethod('show',
+          signature = 'SA',
+          definition = function(object) {
+            line0 = c("Algorithm: Simulated Annealing")
+            line1 = paste0(
+              "Total Run Time: ",
+              round(object@runtime[[1]], 3), 
+              " ", 
+              attr(object@runtime, "units"),
+              "\n"
+            )
+            line2 = suppressWarnings(
+              stringr::str_wrap(
+                as.vector(c("Function call:\n", object@function_call, "\n")), 
+                exdent = 2
+              )
+            )
+            line3 = paste0(
+              stringr::str_wrap(
+                c("Final Model Syntax:", 
+                  unlist(strsplit(object@best_syntax, '\n'))), 
+                exdent = 2), 
+              collapse = "\n"
+            )
+            to_console = paste0(c(line0, line1, line2, line3), collapse = "\n")
+            cat(to_console)
+          }
+)
+
+#' Plot method for class `SA`
+#' @export
+setMethod('plot',
+          signature = 'SA',
+          definition = function(x, y, type = 'all', ...) {
+            val <- data.frame(
+              "Iteration" = 1:length(x@all_fit),
+              "Fit" = as.numeric(x@all_fit)
+            )
+            
+            
+            plot <-
+              ggplot2::ggplot(val, ggplot2::aes_string(x = "Iteration", y = "Fit")) +
+              ggplot2::geom_line() +
+              ggplot2::ylab(paste0("Model Fit Value: ", names(x@best_fit))) +
+              ggplot2::ggtitle(expression("Changes in Model Fit Value per Iteration")) +
+              ggplot2::theme_classic() +
+              ggplot2::theme(
+                legend.position = "none",
+                plot.title = ggplot2::element_text(
+                  size = 16,
+                  face = "bold",
+                  hjust = .5
+                )
+              )
+            
+            plot
+          }
+          )
+
+#' Summary method for class `SA`
+#' @export
+setMethod('summary',
+          signature = 'SA',
+          definition = function(object) {
+            line0 = c("Algorithm: Simulated Annealing")
+            line1 = paste0(
+              "Total Run Time: ",
+              round(object@runtime[[1]], 3), 
+              " ", 
+              attr(object@runtime, "units"),
+              "\n"
+            )
+            line2 = c(capture.output(print(object@best_model)), "\n")
+            line3 = paste0(
+              stringr::str_wrap(
+                c("\nFinal Model Syntax:", 
+                  unlist(strsplit(object@best_syntax, "\n"))), 
+                exdent = 2), 
+              collapse = "\n"
+            )
+            to_console = paste0(c(line0, line1, line2, line3), collapse = "\n")
+            cat(to_console)
+          }
+)
