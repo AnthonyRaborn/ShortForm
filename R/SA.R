f14fd41588faa5c42ad06cebc2861e35a4ec92fd
Anthony W. Raborn <anthony@raborndecisionsciences.com>
2026-08-13 22:40:16 -0400

    added burn-in SA plot option

--- R/SA.R
+++ R/SA.R
@@ -91,37 +91,75 @@ setMethod('show',
 )
 
 #' Plot method for class `SA`
-#' 
-#' @param x,y An S4 object of class `SA`.
+#'
+#' @description Plots the model fit results from the simulated annealing
+#' algorithm. Up to 8 chains are plotted. Any infinite value in the fit
+#' history (e.g. from a chain's initial model failing to fit) is coerced to
+#' `NA`, excluding it from the plot rather than letting it collapse the
+#' fit-value axis range.
+#'
+#' @param x,y An S4 object of class `SA`. `y` is included for method
+#'  compatibility and is not used.
+#' @param burn_in The number of fit results, starting with the first, to
+#'  discard before plotting -- e.g. to exclude an unstable early period of
+#'  the chain(s), as is common practice for Monte Carlo-style methods. Must
+#'  be a non-negative integer less than the number of recorded steps.
+#'  Default is `0` (no burn-in).
 #' @param ... Not used.
-#' 
+#'
 #' @export
-#' @importFrom graphics legend lines par
+#' @importFrom graphics axis legend lines par
 setMethod('plot',
           signature = 'SA',
-          definition = function(x, y, ...) {
+          definition = function(x, y, burn_in = 0, ...) {
 
             temp <- as.data.frame(x@all_fit)
-            
+
+            if (!is.numeric(burn_in) || length(burn_in) != 1 ||
+                burn_in < 0 || burn_in >= nrow(temp)) {
+              warning(
+                "The burn_in parameter was set incorrectly. ",
+                "It must be a single non-negative integer less than the ",
+                "number of recorded steps. Defaulting to burn_in = 0."
+              )
+              burn_in <- 0
+            }
+
+            chainStep <- seq_len(nrow(temp))
+            if (burn_in > 0) {
+              temp <- temp[-seq_len(burn_in), , drop = FALSE]
+              chainStep <- chainStep[-seq_len(burn_in)]
+            }
+
+            temp[temp == Inf | temp == -Inf] <- NA
+
             availableColors <-
               c("black", "#DF536B", "#61D04F", "#2297E6", "#28E2E5", "#D03AF5", "#EEC21F", "gray62")
-            
+
             par(oma = c(0,0,0,5))
             plot(
-              temp[,1], 
+              chainStep,
+              temp[,1],
               col = availableColors[1],
-              type = 'l', 
+              type = 'l',
               ylim = c(min(temp, na.rm = T), max(temp, na.rm = T)),
               bty = "L",
               main = "Model Fit Results per Chain",
               ylab = "Fit Statistic",
-              xlab = "Chain Step"
+              xlab = "Chain Step",
+              xaxt = 'n'
             )
-            
+            # force whole-number Chain Step labels -- with few plotted steps
+            # (e.g. after a large burn_in) the default axis can otherwise
+            # propose fractional tick marks
+            xTicks <- pretty(chainStep)
+            xTicks <- xTicks[xTicks == round(xTicks)]
+            axis(1, at = xTicks)
+
             if (2 <= ncol(temp) & ncol(temp) <=8 ) {
-              
+
               for (i in 2:ncol(temp)) {
-                lines(temp[,i], col = availableColors[i])
+                lines(chainStep, temp[,i], col = availableColors[i])
               }
               legend(
                 par()$usr[2], par()$usr[4],
